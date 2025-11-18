1b04a0e0d0321b7a8ac1920472946c1e35d5de11
bekirsglm <bekirsglm34@gmail.com git config --global user.email bekirsglm34@gmail.com>
2025-11-18 15:25:36 +0300

    Switch to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/home";
 import About from "./pages/about";
@@ -9,33 +9,23 @@ import Contact from "./pages/contact";
 import Footer from "./components/footer";
 
 const App = () => (
-  <BrowserRouter>
+  <Router>
     <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
       <Navbar />
-
-      {/* Ana içerik */}
       <main className="flex-1">
         <Routes>
-          {/* Ana sayfa */}
           <Route path="/" element={<Home />} />
-
-          {/* Diğer sayfalar */}
-          <Route path="/home" element={<Navigate to="/" replace />} /> {/* /home => / yönlendir */}
+          <Route path="/home" element={<Navigate to="/" replace />} />
           <Route path="/about" element={<About />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/contact" element={<Contact />} />
-
-          {/* Tanımlanmayan route */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
-
-      {/* Footer */}
       <Footer />
     </div>
-  </BrowserRouter>
+  </Router>
 );
 
 export default App;
